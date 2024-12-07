7b50f52c954d58bcc7a0b51d24eaa7145eb87ec3
Hyrum Clawson <hyrumclawson@gmail.com>
2024-12-07 11:05:44 -0700

    add a reason string field to invalid move exception

--- shared/src/main/java/chess/InvalidMoveException.java
+++ shared/src/main/java/chess/InvalidMoveException.java
@@ -13,8 +13,16 @@ public class InvalidMoveException extends Exception {
 
 
     }
+    public String reason;
     public InvalidMoveException(ExceptionType e) {
         this.typeOfException = e;
+        this.reason = switch(e){
+          case INVALIDMOVE -> "Invalid move";
+          case WRONGTURN -> "Not your turn";
+          case STARTNULL -> "Trying to move from empty square";
+          default -> "Other issue making a move";
+        };
+
     }
 
     public InvalidMoveException(String message) {
